d5d7c83c28751259cdc257b676da31b410fe9064
pbusenius <14203989+pbusenius@users.noreply.github.com>
2024-12-25 21:43:13 +0100

    added result struct

--- cmd/main.go
+++ cmd/main.go
@@ -6,17 +6,24 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type result struct {
+	url string `json:"url"`
+}
+
 func main() {
 	c := colly.NewCollector()
 
-	// Find and visit all links
+	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
 	})
 
+	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.Visit("http://go-colly.org/")
+	c.Visit("https://www.google.de/search?q=k25+augsburg")
 }
