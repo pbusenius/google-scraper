04c1b56cc143658dadc2c05b7b3e6c77411cac07
pbusenius <14203989+pbusenius@users.noreply.github.com>
2024-12-26 22:00:19 +0100

    added export function

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
+type content struct {
+	Results []result `json:"results"`
+}
+
 type result struct {
-	url         string `json:"url"`
-	description string `json:"description"`
-	query       string `json:"query"`
+	Url         string `json:"url"`
+	Description string `json:"description"`
+	Query       string `json:"query"`
+}
+
+func export(content content, file string) error {
+	rankingsJson, err := json.Marshal(content)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(file, rankingsJson, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
+	var results []result
 	c := colly.NewCollector()
 
 	// On every a element which has href attribute call callback
@@ -28,4 +48,14 @@ func main() {
 	})
 
 	c.Visit("https://www.google.de/search?q=k25+augsburg")
+
+	results = append(results, result{Url: "test", Description: "test", Query: "test"})
+	var content = content{Results: results}
+
+	fmt.Println(content)
+
+	err := export(content, "test.json")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
